f1d8d3a9ca3d8c2fe24cb7a07f0add6f48303522
minsun <kor3334@naver.com>
2021-03-07 23:38:50 +0900

    검색 기능 확장 (하이라이트 기능, 작성자 검색)

--- public/js/script.js
+++ public/js/script.js
@@ -36,3 +36,36 @@ $(function(){
     convertDate();
     convertDateTime();
   });
+
+  $(function(){
+    var search = window.location.search; // 1
+    var params = {};
+  
+    if(search){ // 2
+      $.each(search.slice(1).split('&'),function(index,param){
+        var index = param.indexOf('=');
+        if(index>0){
+          var key = param.slice(0,index);
+          var value = param.slice(index+1);
+  
+          if(!params[key]) params[key] = value;
+        }
+      });
+    }
+  
+    if(params.searchText && params.searchText.length>=3){ // 3
+      $('[data-search-highlight]').each(function(index,element){
+        var $element = $(element);
+        var searchHighlight = $element.data('search-highlight');
+        var index = params.searchType.indexOf(searchHighlight);
+  
+        if(index>=0){
+          var decodedSearchText = params.searchText.replace(/\+/g, ' '); //  3-1
+          decodedSearchText = decodeURI(decodedSearchText);
+          
+          var regex = new RegExp(`(${decodedSearchText})`,'ig'); // 3-2
+          $element.html($element.html().replace(regex,'<span class="highlighted">$1</span>'));
+        }
+      });
+    }
+  });
